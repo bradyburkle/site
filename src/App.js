327ea416c831271ebb9a64440cf6197565d254dd
Brady Burkle <brady.burkle@gmail.com>
2019-09-04 14:46:37 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
+import MainContainer from './containers/MainContainer/MainContainer'
+
+function App() {
+  return (
+    <MainContainer />
+  );
+}
+
+export default App;
