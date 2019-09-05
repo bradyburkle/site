3a290207a234fc5dc63d59a1d26d71af6a6dcc27
Brady Burkle <brady.burkle@gmail.com>
2019-09-04 19:51:18 -0700

    Add files via upload

---
+++ src/data/allJobs.js
@@ -0,0 +1,105 @@
+
+
+const allJobs = {
+    nt: {
+      logo: "img src",
+      firmName: "Northern Trust",
+      roles: {
+        specialist: {
+          title: "Specialist",
+          timeStart: "Aug 2017",
+          timeEnd: "Feb 2018",
+          highlights: ["Good thing 1", "Good thing 2", "Good thing three"],
+          skills: ["Python", "D Slangin"],
+          responsibilities: [
+            "I used this and this to do this",
+            "I also used this and this to do this."
+          ],
+          summary: "I did this.  I also did that.  Pay me."
+        },
+        fundAccounting: {
+          title: "Fund Accounting",
+          timeStart: "Aug 2017",
+          timeEnd: "Feb 2018",
+          highlights: ["Good thing 1", "Good thing 2", "Good thing three"],
+          skills: ["Python", "D Slangin"],
+          responsibilities: [
+            "I used this and this to do this",
+            "I also used this and this to do this."
+          ],
+          summary: "I did this.  I also did that.  Pay me."
+        },
+        investorServices: {
+          title: "Investor Services",
+          timeStart: "Aug 2017",
+          timeEnd: "Feb 2018",
+          highlights: ["Good thing 1", "Good thing 2", "Good thing three"],
+          skills: ["Python", "D Slangin"],
+          responsibilities: [
+            "I used this and this to do this",
+            "I also used this and this to do this."
+          ],
+          summary: "I did this.  I also did that.  Pay me."
+        }
+      },
+      jobTags: [
+        "Finance",
+        "Hedge Funds",
+        "Accounting",
+        "Client Relations",
+        "Investor Relations",
+        "Master Feeder Structures",
+        "Software Testing",
+        "Software Deployment",
+        "Business Analysis",
+        "Systems Analysis",
+        "Automation",
+        "Programming",
+        "Fixed Income",
+        "Equities",
+        "Commodities",
+        "Audit",
+        "Debt",
+        "Collateralized Loan Agreements",
+        "Subscription Documents",
+        "Legal Formation Documentions",
+        "Asset Backed Securities",
+        "Reverse Repos",
+        "Repos",
+        "Clearing",
+        "Treasury Operations"
+      ]
+    },
+    ahc: {
+      logo: "src for logo",
+      firmName: "Anderson Hill Capital LLC",
+      roles: {
+        consultant: {
+          title: "Consultant",
+          timeStart: "Aug 2017",
+          timeEnd: "Feb 2018",
+          highlights: ["Good thing 1", "Good thing 2", "Good thing three"],
+          skills: ["Python", "D Slangin"],
+          responsibilities: [
+            "I used this and this to do this",
+            "I also used this and this to do this."
+          ],
+          summary: "I did this.  I also did that.  Pay me."
+        },
+        fundOps: {
+          title: "Director of Fund Operations",
+          timeStart: "Aug 2017",
+          timeEnd: "Feb 2018",
+          highlights: ["Good thing 1", "Good thing 2", "Good thing three"],
+          skills: ["Python", "D Slangin"],
+          responsibilities: [
+            "I used this and this to do this",
+            "I also used this and this to do this."
+          ],
+          summary: "I did this.  I also did that.  Pay me."
+        }
+      }
+    }
+  };
+  
+  export default allJobs;
---
+++ src/data/allSkills.js
@@ -0,0 +1,60 @@
+
+
+export const listSkills = [
+    'Excel',
+    'Javascript',
+    'React',
+    'Redux',
+    'Python',
+    'VBA',
+    'R'
+]
+
+export const financeSkills = [
+    'Accounting',
+    'Tax',
+    'Investor Reporting',
+    'Regulatory Reporting',
+    'Business Systems',
+    'Consulting',
+    'Investor Relations',
+    'Client Relations',
+    'Financial Analysis',
+    'Financial Analytics',
+    'Financial Modeling',
+    'Data Analysis',
+    'Business Intelligence',
+    'Financial Reporting',
+    'Audit',
+    'GAAP',
+    'Partnership Accounting',
+    'Financial Products',
+    'Legal Documents',
+    'Subscriptions Agreements',
+    'Formation Docs',
+    'Debt Covenenants and Loan Docs'
+]
+
+export const programmingSkills = [
+    
+]
+
+export const financeProductKnowledge = [
+    'Equities',
+    'Fixed Income',
+    'Commodities',
+    ''
+]
+
+export const allSkills = {
+    excel: {
+        img: '',
+        exp: '',
+        skill: '',
+    },
+    word: {
+        img: '',
+        exp: '',
+        skill: '',
+    }
+};
